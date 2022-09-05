e45c8007e34ed8ceeaf92ed96e6b715e15294fb9
Bangyuhyun <asdasdwwqq@naver.com>
2022-09-05 21:25:40 +0900

    modify main.cc

--- main.cc
+++ main.cc
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout << "Hello, world!" << endl;
+	int num;
+	cin >> num;
+	int count = count_(num);
+	cout << "Hello, world!" << count << " " << endl;
+
 	return 0;
 }
